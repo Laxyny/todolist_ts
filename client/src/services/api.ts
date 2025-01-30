10ce388f92bd70da1bdc290740667e48aa036a25
Laxyny <kevgreg30@gmail.com>
2025-01-30 09:50:39 +0100

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -5,8 +5,8 @@ const API_URL = 'http://localhost:3001/api';
 export const api = {
     
     // Auth
-    register: (email: string, password: string) => 
-        axios.post(`${API_URL}/auth/register`, { email, password }),
+    register: (email: string, password: string, displayName: string) => 
+        axios.post(`${API_URL}/auth/register`, { email, password, displayName }),
     
     login: (email: string, password: string) =>
         axios.post(`${API_URL}/auth/login`, { email, password }),
